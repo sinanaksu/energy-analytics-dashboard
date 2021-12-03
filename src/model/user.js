58a7bdcaeb40a0d05cb65f5ee9f794a84ee6c153
Sinan Aksu <sinanksu@gmail.com>
2021-12-03 15:23:27 +0300

    feat: add model/user.js

---
+++ src/model/user.js
@@ -0,0 +1,8 @@
+export default class User {
+  constructor(email, name, password, role) {
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.role = role;
+  }
+}
