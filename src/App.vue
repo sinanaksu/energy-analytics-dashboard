c66d2a6b80df8ba69631e9346082bccd94907a17
Sinan Aksu <sinanksu@gmail.com>
2021-12-03 15:17:00 +0300

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./views/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+#app {
+  font-family: "Rubik", sans-serif !important;
+}
+</style>
---
+++ src/views/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <nav
+      class="navbar navbar-expand-lg bg-nav navbar-light"
+    >
+      <div class="container">
+        <router-link class="navbar-brand" to="/">
+          <img src="../assets/logo.png" width="100" />
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbars1"
+          aria-controls="navbars1"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbars1">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Dashboard</router-link
+              >
+            </li>
+          </ul>
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="dropdownUser"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                >User</a
+              >
+              <ul class="dropdown-menu" aria-labelledby="dropdownUser">
+                <li><a class="dropdown-item" href="#">Setting</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">{{ $t("logout") }}</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container-fluid bg-sub-bar p-2 mb-4">
+      <div class="container">
+        <div class="row">
+          <div class="col"> {{ $route.name }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  components: {},
+};
+</script>
+<style scoped>
+.bg-nav {
+  background: #f1f8fe;
+}
+.bg-sub-bar {
+  background: #7dd5bd;
+}
+</style>
