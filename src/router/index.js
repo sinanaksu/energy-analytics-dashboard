af4c19bd8f6b1dae640ff07ff5199a1f1b1c86d5
Sinan Aksu <sinanksu@gmail.com>
2021-12-03 16:04:51 +0300

    fix: import path at router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
-import Register from './views/Register.vue';
-import Login from './views/Login.vue';
+import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(Router);
 
