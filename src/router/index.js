9522f5a0ff93553fb935ee3ff0e97e35b535bb0e
Sinan Aksu <sinanksu@gmail.com>
2021-12-03 15:24:21 +0300

    feat: add register to routerss

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
+import Register from './views/Register.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Dashboard',
       component: Home
-    }
+    },
+    {
+      path: '/register',
+      component: Register
+    },
   ]
 });
