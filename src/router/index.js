9ab5e1ddf0ce534f2fd90136647d6d9f50b205e9
Sinan Aksu <sinanksu@gmail.com>
2021-12-03 15:26:08 +0300

    feat: add login to routers

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
 import Register from './views/Register.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,10 @@ export default new Router({
     {
       path: '/register',
       component: Register
+    },
+    {
+      path: '/login',
+      component: Login
     },
   ]
 });
