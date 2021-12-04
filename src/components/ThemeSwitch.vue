c86411e07ebb0ff0870f0cf1bb5d457d45f45b42
Sinan Aksu <sinanksu@gmail.com>
2021-12-04 12:52:56 +0300

    feat: create theme switch component

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span type="button" @click="switchRun()" > {{ switchTitle }} </span>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ThemeSwitch',
+  methods:{
+    switchRun() {
+     this.$store.state.darkmode ? this.$store.state.darkmode = false : this.$store.state.darkmode = true
+    }
+  },
+  computed: {
+    switchTitle: function() {
+      return this.$store.state.darkmode ? this.$t('lightmode') : this.$t('darkmode')
+    }
+  }
+}
+</script>
