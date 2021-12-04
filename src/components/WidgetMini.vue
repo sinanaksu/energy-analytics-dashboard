7d064f48bf67ff4dc2f5f303b1efe042aadfc91f
Sinan Aksu <sinanksu@gmail.com>
2021-12-05 02:31:03 +0300

    feat: create widget mini component

---
+++ src/components/WidgetMini.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card card-bg">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ desc }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WidgetMini",
+  props: {
+    title: String,
+    desc: String,
+    type: Number,
+  },
+};
+</script>
+<style scoped>
+.card-bg {
+  background: linear-gradient(90deg,rgb(118, 194, 27) 0%,rgb(167, 218, 66) 100%) !important;
+  box-shadow: 1px 1.732px 60px 0px rgb(0 0 0 / 10%);
+  font-family: "Rubik", sans-serif;
+}
+</style>
