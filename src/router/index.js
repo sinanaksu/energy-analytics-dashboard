96d66a0c0485c092d2949572b43bdeb9eb6f6769
Sinan Aksu <sinanksu@gmail.com>
2021-12-05 02:32:45 +0300

    feat: add auth meta router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,62 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '../views/Home.vue';
-import Register from '../views/Register.vue';
-import Login from '../views/Login.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/Home.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'Dashboard',
-      component: Home
+      path: "/",
+      name: "Dashboard",
+      component: Home,
+      meta: { requiresAuth: true }
     },
     {
-      path: '/register',
-      component: Register
+      path: "/register",
+      component: Register,
+      meta: { guest: true }
     },
     {
-      path: '/login',
-      component: Login
+      path: "/login",
+      component: Login,
+      meta: { guest: true }
+
     },
-  ]
+  ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (localStorage.getItem("user") == null) {
+      next({
+        path: "/login",
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      const user = JSON.parse(localStorage.getItem("user"));
+      if (to.matched.some((record) => record.meta.is_admin)) {
+        if (user.role == 1) {
+          next();
+        } else {
+          next({ name: "Dashboard" });
+        }
+      } else {
+        next();
+      }
+    }
+  } else if (to.matched.some((record) => record.meta.guest)) {
+    if (localStorage.getItem("user") == null) {
+      next();
+    } else {
+      next({ name: "Dashboard" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
