a359f7a6e5e8859c9762a690a8d744ce33530f57
Sinan Aksu <sinanksu@gmail.com>
2021-12-04 13:41:28 +0300

    feat: create locale switch component

---
+++ src/components/LocaleSwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span type="button" @click="switchRun()" > {{ switchTitle }} </span>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'LocaleSwitch',
+  methods:{
+    switchRun() {
+     this.$i18n.locale == 'en' ? this.$i18n.locale = 'tr' : this.$i18n.locale = 'en'
+    }
+  },
+  computed: {
+    switchTitle: function() {
+      return this.$i18n.locale == 'en' ? 'Türkçe' : 'English'
+    }
+  }
+}
+</script>
