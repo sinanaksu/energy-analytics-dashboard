4947c4b89e1153b5ad48812aa80fdb3f765cb882
Sinan Aksu <sinanksu@gmail.com>
2021-12-06 04:41:47 +0300

    style: add dark mode css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:class="{ dark: this.$store.state.darkmode }">
     <Navbar v-show="navbarShow" />
     <div class="container">
         <router-view />
@@ -30,5 +30,53 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 #app {
   font-family: "Rubik", sans-serif !important;
+  background-image: linear-gradient(100deg, #c1dfc4 0%, #deecdd 100%);
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+  position: fixed;
+}
+
+:root {
+  @import "../node_modules/bootstrap/scss/functions";
+  @import "../node_modules/bootstrap/scss/variables";
+  @import "../node_modules/bootstrap/scss/mixins";
+
+  .dark {
+    background-image: none !important;
+    background-color: rgb(112, 112, 112);
+
+    $enable-gradients: true;
+    $primary: #4e7296;
+    $secondary: #afafaf;
+    $success: #6d8a76;
+    $dark: #000;
+    $danger: rgb(116, 77, 77);
+    $white:    rgb(133, 30, 30);
+
+    $theme-colors: (
+      "white": $white,
+      "primary": $primary,
+      "secondary": $secondary,
+      "success": $success,
+      "danger": $danger,
+      "dark": $dark,
+      "light": #aaa,
+    );
+
+    $theme-colors-rgb: map-loop($theme-colors, to-rgb, "$value");
+
+    @each $color, $value in $theme-colors-rgb {
+      --#{$variable-prefix}#{$color}-rgb: #{$value};
+    }
+
+    $body-color: #eeeeee;
+    $body-bg: #263c5c;
+
+    --#{$variable-prefix}body-color: #{$body-color};
+    --#{$variable-prefix}body-bg: #{$body-bg};
+    @import "bootstrap";
+  }
 }
 </style>
