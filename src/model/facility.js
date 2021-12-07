ceefb06bca0d30024488d680b1bffb6467d6df83
Sinan Aksu <sinanksu@gmail.com>
2021-12-07 04:35:54 +0300

    feat: add facility model

---
+++ src/model/facility.js
@@ -0,0 +1,10 @@
+export default class Facility {
+  constructor(id = 0, name = '', start_date = '', end_date = '', workers = '', special = true,) {
+    this.id = id,
+    this.name = name;
+    this.start_date = start_date;
+    this.service_end = end_date;
+    this.workers = workers;
+    this.special = special;
+  }
+}
