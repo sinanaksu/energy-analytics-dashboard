038abd69b346d5a326592a2a4656565b8d430e91
Sinan Aksu <sinanksu@gmail.com>
2021-12-07 04:36:29 +0300

    feat: add facility service

---
+++ src/service/facility.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import authHeader from "../utils/authHeader";
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+export const getAll = () => {
+  return axios.get(API_URL + "facility/", { headers: authHeader() });
+};
+
+export const create = (facility) => {
+  return axios.post(API_URL + "facility/", facility, { headers: authHeader() });
+};
+
+export const update = (facility) => {
+  return axios.put(API_URL + "facility/", facility, { headers: authHeader() });
+};
