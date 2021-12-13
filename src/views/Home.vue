32b9031c9f7fe6874c19747f1c2f019f5ce8fa06
Sinan Aksu <sinanksu@gmail.com>
2021-12-13 17:30:00 +0300

    feat: add charts to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-3">
-        <WidgetMini :desc="$t('analystics.daymw')" :title="data.day.mw + ' mW'" type="1" />
+      <div class="col-sm-12 col-md-3 col-lg-3 mb-4">
+        <WidgetMini :desc="$t('analystics.daymw')" :title="data.day.mw + ' mW'" :type="1" />
       </div>
-      <div class="col-3">
-        <WidgetMini :desc="$t('analystics.dayhz')" :title="data.day.hz + ' Hz'" type="1" />
+      <div class="col-sm-12 col-md-3 col-lg-3 mb-4">
+        <WidgetMini :desc="$t('analystics.dayhz')" :title="data.day.hz + ' Hz'" :type="1" />
       </div>
-      <div class="col-3">
-        <WidgetMini :desc="$t('analystics.dayvolt')" :title="data.day.volt + ' Volt'" type="1" />
+      <div class="col-sm-12 col-md-3 col-lg-3 mb-4">
+        <WidgetMini :desc="$t('analystics.dayvolt')" :title="data.day.volt + ' Volt'" :type="1" />
       </div>
-      <div class="col-3">
-        <WidgetMini :desc="$t('analystics.dayamper') " :title="data.day.amper + ' Amper'" type="1" />
+      <div class="col-sm-12 col-md-3 col-lg-3 mb-4">
+        <WidgetMini :desc="$t('analystics.dayamper') " :title="data.day.amper + ' Amper'" :type="1" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12 mb-4">
+        <Heatmap />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
+        <Series type="frequency" />
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
+        <Series type="phases" />
       </div>
     </div>
   </div>
@@ -19,17 +32,21 @@
 
 <script>
 import WidgetMini from "../components/WidgetMini.vue";
+import Heatmap from "../components/Highcharts/Heatmap.vue"
+import Series from "../components/Highcharts/Series.vue"
 import { day } from "../service/analystics";
 
 export default {
   name: "Home",
   components: {
     WidgetMini,
+    Heatmap,
+    Series
   },
   data() {
     return {
       data: {},
-      loading: true
+      loading: true,
     };
   },
   mounted() {
