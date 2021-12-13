986bcf089ec7b84648651730c067a32147640db8
Sinan Aksu <sinanksu@gmail.com>
2021-12-13 14:08:34 +0300

    feat: add heatmap, series to analystic service

--- src/service/analystics.js
+++ src/service/analystics.js
@@ -4,6 +4,17 @@ import authHeader from "../utils/authHeader";
 const API_URL = process.env.VUE_APP_API_URL;
 
 export const day = () => {
-  console.log(authHeader());
   return axios.get(API_URL + "analytics/day/", { headers: authHeader() });
 };
+
+export const heatmapData = () => {
+  return axios.get(API_URL + "analytics/heatmap/", { headers: authHeader() });
+};
+
+export const seriesData = (t) => {
+  return axios.get(API_URL + "analytics/series/" + t + "/", { headers: authHeader() });
+};
+
+
+
+
